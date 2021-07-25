b8d6f0b839aae2fd8d67330cca32f7e1a6840310
hebaarch <82498929+hebaarch@users.noreply.github.com>
2021-07-25 12:47:04 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,9 @@ import webbrowser
 
 
 download_model(model='bert-squad_1.1', dir='./models')
+from pathlib import Path
+p = Path('.')
+[x for x in p.iterdir() if x.is_dir()]
 df = pdf_converter(directory_path="docs/FIRE AND LIFE SAFETY CODE_Page24.pdf")
 
 print(df.head())
