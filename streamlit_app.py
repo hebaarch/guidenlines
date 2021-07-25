cc0e18b44d1143eee74a138cd4a47e0d99665972
hebaarch <82498929+hebaarch@users.noreply.github.com>
2021-07-25 15:00:28 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,11 @@ df = pdf_converter(directory_path = docs_path)
 print(df.head())
 
 pd.set_option('display.max_colwidth', -1)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-cdqa_pipeline = QAPipeline(reader='./models/bert_qa.joblib', max_df=1.0)
+models = os.path.join(BASE_DIR, "models")
+
+cdqa_pipeline = QAPipeline(reader='models', max_df=1.0)
 
 cdqa_pipeline.fit_retriever(df=df)
 
